535a9c420f4207fae37948ed053357843e1a8c06
CubicStone31 <CubicStone31@gmail.com>
2021-09-15 20:54:05 +0800

    gui setup

--- MyInjector/MainWindow.xaml.cs
+++ MyInjector/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace MyInjector
 {
@@ -23,6 +24,46 @@ namespace MyInjector
         public MainWindow()
         {
             InitializeComponent();
+            cbx_process_list.DataContext = processListSource;
+        }
+
+        private void cbx_process_list_DropDownOpened(object sender, EventArgs e)
+        {
+            cbx_process_list.GetBindingExpression(ComboBox.ItemsSourceProperty).UpdateTarget();
+        }
+
+        private ProcessListSource processListSource = new ProcessListSource();
+
+        private int GetTargetPID()
+        {
+            string data = cbx_process_list.SelectedItem as string;
+            data = data.Substring(0, data.IndexOf('\t'));
+            return int.Parse(data);
+        }
+    }
+
+    public class ProcessListSource
+    {
+        public IEnumerable<string> ProcessList
+        {
+            get
+            {
+                var ret = new List<string>();
+                var plist = Process.GetProcesses();
+                Array.Sort(plist, delegate (Process process1, Process process2)
+                {
+                    return process1.Id.CompareTo(process2.Id);
+                });
+
+                foreach (var process in plist)
+                {
+                    var pid = process.Id;
+                    var pname = process.ProcessName;
+                    var content = String.Format("{0}\t{1}", pid, pname);
+                    ret.Add(content);
+                }
+                return ret;
+            }
         }
     }
 }
